fe59ae3e5f580d4c64af9314011a348ec00e6f5c
Svatosik <125908175+Svatosik@users.noreply.github.com>
2025-04-26 18:39:15 +0200

    Normalize imports by adding index.jsx to components directories

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import "normalize.css";
 
 //components
 import { Component } from "react";
-import { Section } from "./components/Section/Section";
-import { ContactForm } from "./components/ContactForm/ContactForm";
-import { ContactList } from "./components/ContactList/ContactList";
-import { Filter } from "./components/Filter/Filter";
+import { Section } from "./components/Section";
+import { ContactForm } from "./components/ContactForm";
+import { ContactList } from "./components/ContactList";
+import { Filter } from "./components/Filter";
 //libraries
 import { nanoid } from "nanoid";
 
